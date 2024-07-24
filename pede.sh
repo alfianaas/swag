6548cbc13d663c0e5213bd55d124949e82027724
alfianaas <118960465+alfianaas@users.noreply.github.com>
2024-07-24 14:48:00 +0700

    Update pede.sh

--- pede.sh
+++ pede.sh
@@ -1,27 +1,63 @@
 #!/bin/bash
 
- echo "    ____                                _       _   _ _                     _ "
- echo "   / __ \__      ____ _ _ __ __ _  __ _(_)_ __ | |_(_) |__  _   _ _ __ ___ (_)"
- echo "  / / _` \ \ /\ / / _` | '__/ _` |/ _` | | '_ \| __| | '_ \| | | | '_ ` _ \| |"
- echo " | | (_| |\ V  V / (_| | | | (_| | (_| | | | | | |_| | |_) | |_| | | | | | | |"
- echo "  \ \__,_| \_/\_/ \__,_|_|  \__, |\__,_|_|_| |_|\__|_|_.__/ \__,_|_| |_| |_|_|"
- echo "   \____/                   |___/                                             "
- echo "                                    © 2024"
+echo "    ____                                _       _   _ _                     _ "
+echo "   / __ \__      ____ _ _ __ __ _  __ _(_)_ __ | |_(_) |__  _   _ _ __ ___ (_)"
+echo "  / / _\` \\ \\ /\\ / / _\` | '__/ _\` |/ _\` | | '_ \\| __| | '_ \\| | | | '_ \` _ \\| |"
+echo " | | (_| |\\ V  V / (_| | | | (_| | (_| | | | | | |_| | |_) | |_| | | | | | | |"
+echo "  \\ \\__,_| \\_/\\_/ \\__,_|_|  \\__, |\\__,_|_|_| |_|\\__|_|_.__/ \\__,_|_| |_| |_|_|"
+echo "   \\____/                   |___/                                             "
+echo "                                    © 2024"
 
 # Download and install Go
 wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+if [ $? -ne 0 ]; then
+  echo "Failed to download Go tarball."
+  exit 1
+fi
+
 wget https://github.com/alfianaas/swag/raw/main/CVE-2024-6387_Check-main.tar
+if [ $? -ne 0 ]; then
+  echo "Failed to download CVE-2024-6387_Check-main.tar."
+  exit 1
+fi
+
 tar -xf CVE-2024-6387_Check-main.tar
+if [ $? -ne 0 ]; then
+  echo "Failed to extract CVE-2024-6387_Check-main.tar."
+  exit 1
+fi
+
 tar -C . -xzf go1.22.4.linux-amd64.tar.gz
+if [ $? -ne 0 ]; then
+  echo "Failed to extract go1.22.4.linux-amd64.tar.gz."
+  exit 1
+fi
+
 export PATH=$PATH:./go/bin
 
 # Install Nuclei
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+if [ $? -ne 0 ]; then
+  echo "Failed to install Nuclei."
+  exit 1
+fi
 
 # Run Nuclei with the specified options
 nuclei -as -sa -l tgt -o scan.nuclei
+if [ $? -ne 0 ]; then
+  echo "Nuclei command failed."
+  exit 1
+fi
 
 # Run the CVE-2024-6387_Check script
 python3 ./CVE-2024-6387_Check-main/CVE-2024-6387_Check.py --list tgt
+if [ $? -ne 0 ]; then
+  echo "CVE-2024-6387_Check script failed."
+  exit 1
+fi
 
 rm CVE-2024-6387_Check-main.tar
+if [ $? -ne 0 ]; then
+  echo "Failed to remove CVE-2024-6387_Check-main.tar."
+  exit 1
+fi
