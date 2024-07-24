b8baee787ac73a1c9b7cdd1b6e5936de395ce7b8
alfianaas <118960465+alfianaas@users.noreply.github.com>
2024-07-24 14:18:53 +0700

    Update pede.sh

--- pede.sh
+++ pede.sh
@@ -2,17 +2,18 @@
 
 # Download and install Go
 wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+wget https://github.com/alfianaas/swag/raw/main/CVE-2024-6387_Check-main.tar
+tar -xf CVE-2024-6387_Check-main.tar
 tar -C . -xzf go1.22.4.linux-amd64.tar.gz
 export PATH=$PATH:./go/bin
 
 # Install Nuclei
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 
-# Clone the CVE-2024-6387_Check repository
-git clone https://github.com/xaitax/CVE-2024-6387_Check.git
-
 # Run Nuclei with the specified options
 nuclei -as -sa -l tgt -o scan.nuclei
 
 # Run the CVE-2024-6387_Check script
-python3 ./CVE-2024-6387_Check/CVE-2024-6387_Check.py --list tgt
+python3 ./CVE-2024-6387_Check-main/CVE-2024-6387_Check.py --list tgt
+
+rm CVE-2024-6387_Check-main.tar
